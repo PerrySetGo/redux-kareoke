b104caece7a7b237af0b3fed7981b16a0f2d5bc8
perry eising <perry.r.eising@gmail.com>
2018-06-21 15:08:12 -0700

    complete kareoke exercise with two actions and passing tests

--- scripts.js
+++ scripts.js
@@ -7,13 +7,17 @@ const initialState = {
 }
 //reducer here
 const reducer = (state = initialState, action) =>{
+  let newState
   switch(action.type) {
     case 'NEXT_LYRIC':
-    let newArrayPosition = state.arrayPosition + 1; //advance the counter
-    let newState = { //get the new state
-      songLyricsArray:state.songLyricsArray,
-      arrayPosition:newArrayPosition
-    }
+      let newArrayPosition = state.arrayPosition + 1; //advance the counter
+      newState = { //get the new state
+        songLyricsArray:state.songLyricsArray,
+        arrayPosition:newArrayPosition
+      }
+      return newState;
+    case 'RESTART_SONG':
+      newState = initialState;
       return newState;
     default:
         return state;
@@ -29,8 +33,16 @@ expect(reducer(initialState, {type: null})).toEqual(initialState); //not mutatin
 expect(reducer(initialState, {type: 'NEXT_LYRIC'})).toEqual({
   songLyricsArray:songLyricsArray,
   arrayPosition:1
+});
 
-})
+expect(reducer({
+  songLyricsArray:songLyricsArray,
+  arrayPosition:1
+},
+{ type:'RESTART_SONG'})
+).toEqual(initialState);
+
+
 
 //redux store
 
@@ -38,6 +50,31 @@ const { createStore } = Redux;
 const store = createStore(reducer);
 console.log(store.getState());
 
-const userClick = () => {
-  console.log('click');
+const renderLyrics = () => {
+  const lyricsDisplay = document.getElementById('lyrics');
+  // if there are already lyrics in this div, remove them one-by-one until it is empty:
+  while(lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild)
+  }
+
+  const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
+  const renderedLine = document.createTextNode(currentLine);
+  document.getElementById('lyrics').appendChild(renderedLine);
 }
+
+window.onload = function(){
+  renderLyrics();
+}
+
+//UI
+
+const userClick = () => {
+  const currentState = store.getState();
+  if (currentState.arrayPosition === currentState.songLyricsArray.length - 1) {
+    store.dispatch({ type: 'RESTART_SONG' } );
+  } else {
+    store.dispatch({ type: 'NEXT_LYRIC' } );
+  }
+}
+
+store.subscribe(renderLyrics);
