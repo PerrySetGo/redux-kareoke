f74e7fb34db0c5f2bc760b490bab4a52fbbbb353
perry eising <perry.r.eising@gmail.com>
2018-06-21 22:01:36 -0700

    add additional lyrics for second song

--- scripts.js
+++ scripts.js
@@ -1,9 +1,26 @@
-const songLyricsArray = "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye".split(', ');
-
+const const songList = {
+  1: "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye".split(', '),
+  2: "I'm so into you, I can barely breathe, And all I wanna do is to fall in deep, But close ain't close enough 'til we cross the line, So name a game to play, and I'll roll the dice, hey, Oh baby, look what you started, The temperature's rising in here, Is this gonna happen?, Been waiting and waiting for you to make a move, Before I make a move".split(', ')
+};
 // INITIAL REDUX STATE
 const initialState = {
-  songLyricsArray: songLyricsArray,
-  arrayPosition: 0,
+  currentSongId:null,
+  songsById: {
+    1: {
+      title: "Bye Bye Bye",
+      artist: "N'Sync",
+      songId: 1,
+      songArray:songList[1],
+      arrayPosition:0
+    },
+    2: {
+      title: "Into You",
+      artist: "Ariana Grande",
+      songId:2,
+      songArray: songList[2],
+      arrayPosition:0
+    }
+  }
 }
 //reducer here
 const reducer = (state = initialState, action) =>{
